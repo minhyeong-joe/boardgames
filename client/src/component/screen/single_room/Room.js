9c74881404a8ed1b5d5235e44360a1ef0e9fe362
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2021-02-19 00:43:24 -0800

    set up and test socket io

--- client/src/component/screen/single_room/Room.js
+++ client/src/component/screen/single_room/Room.js
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import io from "socket.io-client";
+
+let socket;
 
 const Room = ({ match }) => {
+	const ENDPOINT = "localhost";
+	const auth = useSelector((state) => state.auth);
+
+	useEffect(() => {
+		socket = io(ENDPOINT);
+		console.log(socket);
+
+		socket.emit(
+			"join",
+			{
+				username: auth.userInfo.username,
+				userId: auth.userInfo._id,
+				room: match.params.roomId,
+			},
+			() => {}
+		);
+
+		return () => {
+			socket.off();
+		};
+	}, [
+		ENDPOINT,
+		auth.userInfo._id,
+		auth.userInfo.username,
+		match.params.roomId,
+	]);
+
+	useEffect(() => {
+		socket.on("message", (message) => {
+			console.log(message);
+		});
+	});
+
 	return <div>Room</div>;
 };
 
