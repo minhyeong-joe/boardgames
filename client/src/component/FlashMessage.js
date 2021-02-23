39fa447e7de8e12d2dfd6a221ac5acd2d3000458
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2021-02-23 12:28:40 -0800

    fixed flash message flickering as it disappears

--- client/src/component/FlashMessage.js
+++ client/src/component/FlashMessage.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { IconButton, Snackbar } from "@material-ui/core";
-import { CloseIcon } from "@material-ui/icons/Close";
+import { Snackbar } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 import { useDispatch, useSelector } from "react-redux";
 import { closeFlash } from "../actions";
@@ -14,22 +13,26 @@ const FlashMessage = () => {
 	};
 
 	return (
-		<Snackbar
-			autoHideDuration={flash.duration}
-			anchorOrigin={{
-				vertical: flash.vertical || "top",
-				horizontal: flash.horizontal || "center",
-			}}
-			open={flash.show}
-			onClose={handleClose}
-		>
-			<Alert
-				onClose={flash.closeIcon ? handleClose : null}
-				severity={flash.severity || "success"}
-			>
-				{flash.message}
-			</Alert>
-		</Snackbar>
+		<>
+			{flash.show && (
+				<Snackbar
+					autoHideDuration={flash.duration}
+					anchorOrigin={{
+						vertical: flash.vertical || "top",
+						horizontal: flash.horizontal || "center",
+					}}
+					open={flash.show}
+					onClose={handleClose}
+				>
+					<Alert
+						onClose={flash.closeIcon ? handleClose : null}
+						severity={flash.severity || "success"}
+					>
+						{flash.message}
+					</Alert>
+				</Snackbar>
+			)}
+		</>
 	);
 };
 
